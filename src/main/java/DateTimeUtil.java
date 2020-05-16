b7a3a47587acf478be624f09d4ed5d0558c38602
Bhavesh Bawadhane <bhavesh.bawadhane@niceactimize.com>
2020-05-16 19:36:53 +0530

    refactored the leap year logic

--- src/main/java/DateTimeUtil.java
+++ src/main/java/DateTimeUtil.java
@@ -2,13 +2,6 @@ package leapyear.src.main.java;
 
 public class DateTimeUtil {
     public static boolean isLeapYear(int year) {
-        if (year % 4 == 0){
-            if(year % 100 != 0)
-                return true;
-        }
-        if(year % 400 == 0)
-            return true;
-        else
-            return false;
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
     }
 }
