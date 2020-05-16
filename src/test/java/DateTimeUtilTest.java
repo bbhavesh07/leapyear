fd70337102a521e1649d2d1c2eb27f8c47dd3348
Bhavesh Bawadhane <bhavesh.bawadhane@niceactimize.com>
2020-05-16 19:13:01 +0530

    test for All years divisible by 100 but not by 400 are NOT leap years

--- src/test/java/DateTimeUtilTest.java
+++ src/test/java/DateTimeUtilTest.java
@@ -4,11 +4,19 @@ import leapyear.src.main.java.DateTimeUtil;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class DateTimeUtilTest {
 
     @Test
     public void givenYear_divisible_by_400_returns_true(){
         assertTrue(DateTimeUtil.isLeapYear(2000));
+    }
+
+    @Test
+    public void givenYear_divisible_by_100_and_not_by_400_returns_false(){
+        int arr[] = {1700, 1800, 1900, 2100};
+        for(int year: arr)
+            assertFalse(DateTimeUtil.isLeapYear(year));
     }
 }
