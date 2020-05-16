ee580e730ff2e5202c49456457b0f5471f0bf5ec
Bhavesh Bawadhane <bhavesh.bawadhane@niceactimize.com>
2020-05-16 19:19:24 +0530

    fixed test for givenYear_divisible_by_4_and_not_by_100_returns_true

--- src/main/java/DateTimeUtil.java
+++ src/main/java/DateTimeUtil.java
@@ -1,7 +1,11 @@
 package leapyear.src.main.java;
 
 public class DateTimeUtil {
-    public static boolean isLeapYear(int year){
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0){
+            if(year % 100 != 0)
+                return true;
+        }
         if(year % 400 == 0)
             return true;
         else
